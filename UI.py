5fedf9d549be9115815740ebe70ace386e056acd
Tanishk-2004 <agarwaltanishk73@gmail.com>
2026-08-17 19:11:21 +0530

    Remove the sidebar button

--- UI.py
+++ UI.py
@@ -66,7 +66,7 @@ st.set_page_config(
     page_title="HIT Knowledge Assistant",
     page_icon="🎓",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="locked",
 )
 
 
