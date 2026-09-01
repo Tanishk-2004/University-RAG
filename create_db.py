1f5695652b7f30373ba9c87ce0fd04469e947159
Tanishk-2004 <agarwaltanishk73@gmail.com>
2026-09-01 17:51:30 +0530

    new chroma_db created

--- create_db.py
+++ create_db.py
@@ -1,59 +1,71 @@
-from langchain_community.document_loaders import PyPDFLoader  # Used for loading the pdf
-from pathlib import Path 
-from langchain_text_splitters import RecursiveCharacterTextSplitter  # used for chunking
-from langchain_community.embeddings import HuggingFaceEmbeddings 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-import sys
+from pathlib import Path
+import re
+import hashlib
 
 project_root = Path(__file__).resolve().parent
 
-knowledge_base=project_root/ "knowledge_base"
+knowledge_base = project_root / "knowledge_base"
+
+pdf_files = knowledge_base.glob("*.pdf")
+
+documents = []
 
 
+def get_file_hash(pdf_path):
+    sha256 = hashlib.sha256()
 
-pdf_files=knowledge_base.glob("*.pdf")     # this will store all the files ending with .pdf into the pdf_files variable 
+    with open(pdf_path, "rb") as f:
+        for chunk in iter(lambda: f.read(8192), b""):
+            sha256.update(chunk)
 
-
-
-documents=[]  # TO add all the pages 
-
-# # now we use for loop to deal with pdf one one at a time and also create empty documents list so that later we can add all the pdfs in one place 
-# #  we convert pdf into the string coz the pyPDFloader expects string 
+    return sha256.hexdigest()
 
 
 for pdf in pdf_files:
+
+    file_hash = get_file_hash(pdf)
+
     loader = PyPDFLoader(str(pdf))
     loaded_pages = loader.load()
+
+    for doc in loaded_pages:
+        doc.metadata["document_hash"] = file_hash
+        doc.metadata["scope"] = "global"
+
     documents.extend(loaded_pages)
-    
-# ## To replace the \n coz its causing each word to go to new line and make our text look bad we use this
+
+
 import re
+
 for doc in documents:
-    doc.page_content=re.sub(r"\s+"," ",doc.page_content).strip() 
+    doc.page_content = re.sub(
+        r"\s+",
+        " ",
+        doc.page_content
+    ).strip()
 
 
-text_splitter=RecursiveCharacterTextSplitter(
+text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1300,
     chunk_overlap=450,
     length_function=len,
-    separators=["\n\n","\n"," ",""]  
+    separators=["\n\n", "\n", " ", ""]
 )
 
-chunks = text_splitter.split_documents(documents) # now the dcoument is splitted into smaller smaller chunks 
-
-print(chunks[0].page_content)
+chunks = text_splitter.split_documents(documents)
 
 
-#Embedding Model
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
 path_to_db = project_root / "chroma_db"
 
-print(path_to_db)
-
-vector_store=Chroma.from_documents(
+vector_store = Chroma.from_documents(
     documents=chunks,
     embedding=embedding_model,
     collection_name="University_RAG",
